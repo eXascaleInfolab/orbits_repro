c323ca0a7c027fe91e6d1b45b0436229690d9f3a
Mourad Khayati <mkhayati@exascale.info>
2021-07-02 16:50:39 +0200

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -6,4 +6,4 @@ pip2 install numpy scipy pandas sklearn
 pip2 install tensorflow
 python linux_build.py
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1MlX_3OF3zKLKocFKVWYK5yBOfXHTaDBW' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1MlX_3OF3zKLKocFKVWYK5yBOfXHTaDBW" -O soccer_full_normal.txt && rm -rf /tmp/cookies.txt
-
+mv soccer_full_normal.txt /home/ubuntu/code/orbits_repro/TestingFramework/bin/Debug/data/soccer_full/
