2d4a9bf381afed93c59be19a9b5bb2d69ff3d93b
ZakharTymchenko <ZakharTymchenko@github.com>
2021-07-14 17:30:44 +0200

    fixed paths in ssv tests

--- TestingFramework/bin/Debug/repro_extra_ssv.sh
+++ TestingFramework/bin/Debug/repro_extra_ssv.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Figure 13
-mono TestingFramework.exe ./scrlong/f13_metaorbitsrt_ssv_len.cfg
-python -c 'from scrlong.f13_avger import average; average("length", "soccer", 2);' >> ResultsSVector/f13_len.txt
+mono TestingFramework.exe ./scrextra/f13_metaorbitsrt_ssv_len.cfg
+python -c 'from scrextra.f13_avger import average; average("length", "soccer", 2);' >> ResultsSVector/f13_len.txt
 /bin/echo -e "\n" >> ResultsSVector/f13_len.txt
 
-mono TestingFramework.exe ./scrlong/f13_metaorbitsrt_ssv_col.cfg
-python -c 'from scrlong.f13_avger import average; average("columns", "soccer", 2);' >> ResultsSVector/f13_col.txt
+mono TestingFramework.exe ./scrextra/f13_metaorbitsrt_ssv_col.cfg
+python -c 'from scrextra.f13_avger import average; average("columns", "soccer", 2);' >> ResultsSVector/f13_col.txt
 /bin/echo -e "\n" >> ResultsSVector/f13_col.txt
