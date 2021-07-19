d2ceac5e1b1cf25255a2fd60da6c9a86374cdae1
ZakharTymchenko <ZakharTymchenko@github.com>
2021-07-19 10:20:25 +0200

    undo output file change on extra_ssv

--- TestingFramework/bin/Debug/repro_extra_ssv.sh
+++ TestingFramework/bin/Debug/repro_extra_ssv.sh
@@ -2,9 +2,9 @@
 
 # Figure 13
 mono TestingFramework.exe ./scrextra/f13_metaorbitsrt_ssv_len.cfg
-python -c 'from scrextra.f13_avger import average; average("length", "soccer", 2);' >> ResultsSVector/f13_len_ssv.txt
+python -c 'from scrextra.f13_avger import average; average("length", "soccer", 2);' >> ResultsSVector/f13_len.txt
 /bin/echo -e "\n" >> ResultsSVector/f13_len.txt
 
 mono TestingFramework.exe ./scrextra/f13_metaorbitsrt_ssv_col.cfg
-python -c 'from scrextra.f13_avger import average; average("columns", "soccer", 2);' >> ResultsSVector/f13_col_ssv.txt
+python -c 'from scrextra.f13_avger import average; average("columns", "soccer", 2);' >> ResultsSVector/f13_col.txt
 /bin/echo -e "\n" >> ResultsSVector/f13_col.txt
